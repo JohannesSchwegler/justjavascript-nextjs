0355da58ed5d962335ad4eaddf74b3ed1b25f00a
Johannes Schwegler <johannes.schwegler@outlook.de>
2020-05-21 20:53:05 +0200

    first commit

---
+++ components/header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+      <header>
+        <div className="jj-container">
+          <nav className="jj-nav-grid">
+            <div className="jj-nav-grid-col -left"></div>
+            <div className="jj-nav-grid-col -right"></div>
+          </nav>
+        </div>
+      </header>
+    );
+}
